f83f240052230f7cc613a6bd4d944bc506be8c11
Justin <Justin.Gartenberg@stonybrook.edu>
2018-05-04 13:56:16 -0400

    Working on display latest critic reviews

--- src/main/java/main/java/managers/ReviewManager.java
+++ src/main/java/main/java/managers/ReviewManager.java
@@ -1,8 +1,11 @@
 package main.java.managers;
 
+import java.time.LocalDate;
+import java.util.List;
 import main.java.models.Review;
+import main.java.models.UserReview;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewManager extends CrudRepository<Review, Integer> {
-	
+	public List<UserReview> findTop10ByDateBefore(LocalDate date);
 }
