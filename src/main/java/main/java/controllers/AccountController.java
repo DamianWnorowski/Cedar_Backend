4fc2c2f897a810f319ff80bc66ce0eb0d6118c65
irk <chrisdelvallesi@gmail.com>
2018-05-04 11:38:57 -0400

    delete account

--- src/main/java/main/java/controllers/AccountController.java
+++ src/main/java/main/java/controllers/AccountController.java
@@ -1,8 +1,10 @@
 package main.java.controllers;
 
+import java.util.NoSuchElementException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import main.java.managers.UserManager;
+import main.java.models.ErrorCode;
 import main.java.models.LoginForm;
 import main.java.models.RegistrationForm;
 import main.java.models.User;
@@ -14,10 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("http://localhost:3000")
@@ -80,5 +84,27 @@ public class AccountController {
     public int validateUser(User u, LoginForm lf) {
         return 0;
     }
-
+	
+	@PostMapping("/api/deleteaccount")
+    public ErrorCode deleteAccount(@RequestParam(value="id") int id) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		if (email.equals("anonymousUser")) {
+			return ErrorCode.NOTLOGGEDIN;
+		}
+		User currentUser = um.findByEmail(email);
+		User userToDelete;
+		try {
+			userToDelete = um.findById(id).get();
+		}
+		catch (NoSuchElementException e) {
+			return ErrorCode.DOESNOTEXIST;
+		}
+		
+		if (currentUser.getId() != userToDelete.getId() && currentUser.getRole() != UserRole.ROLE_ADMIN){
+			return ErrorCode.INVALIDPERMISSIONS;
+		}
+		
+		um.delete(userToDelete);
+        return ErrorCode.SUCCESS;
+    }
 }
