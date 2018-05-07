bdb1416837dce8b2903865ed6bf88436bf46d576
unknown <Serge.Crane@stonybrook.edu>
2018-05-07 17:45:08 -0400

    oopsies

--- src/main/java/main/java/dto/DeleteAccountForm.java
+++ src/main/java/main/java/dto/DeleteAccountForm.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main.java.dto;
 
-/**
- *
- * @author Serge
- */
 public class DeleteAccountForm {
+    int id;
+    String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
 }
