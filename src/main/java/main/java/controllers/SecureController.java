67f6a622040440fa98d07a1776026c33ff9fb416
unknown <Serge.Crane@stonybrook.edu>
2018-05-08 17:16:40 -0400

    Reporting Users Use Case Done

--- src/main/java/main/java/controllers/SecureController.java
+++ src/main/java/main/java/controllers/SecureController.java
@@ -27,15 +27,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Serge
- */
+@CrossOrigin("http://localhost:3000")
+@RestController
 public class SecureController {
     @Autowired
     private UserManager userManager;
